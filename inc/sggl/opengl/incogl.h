41ee8482c09f2154ddff488ab527b66fb9bd2f8e
FallingSoul <2123064158@qq.com>
2023-08-15 00:38:28 +0800

    Chore(opengl):添加glew和glad两个选择

--- inc/sggl/opengl/incogl.h
+++ inc/sggl/opengl/incogl.h
@@ -3,8 +3,14 @@
 
 #include "sggl/family.h"
 
+
+#if defined(SG_GL_OPENGL_USE_GLEW)
 #include <GL/glew.h>
-#include <GLFW/glfw3.h>
+#elif defined(SG_GL_OPENGL_USE_GLAD)
+#include <GL/glad.h>
+#else
+#error "choose glew or glad (define SG_GL_OPENGL_USE_GLEW or SG_GL_OPENGL_USE_GLAD)"
+#endif
 
 
 #endif //!__SG_GL_INCOGL__
